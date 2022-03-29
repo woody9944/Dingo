82fb3223d3ebeaf73c1bf0100438dc0a57e64cfe
Daniel <dwoodruffe94@gmail.com>
2022-03-29 16:33:12 +0100

    Add project files.

---
+++ Dingo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Dingo
+{
+    static void Main(string[] args)
+    {
+        String solution = args[0];
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Engine engine = new Engine(solution);
+        System.Diagnostics.Debug.WriteLine(engine.Execute());
+    }
+}
